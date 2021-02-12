eae712410d2eb1caa62fb2bc840486bf3b41da00
ftomi <tomifarkas@yahoo.com>
2021-02-12 09:43:40 +0100

    Eslint + notes

---
+++ app/navigation/NotesNavigator.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// import NoteListScreen from "../screens/NoteListScreen";
+// import NoteAddScreen from "../screens/NoteAddScreen";
+
+const Stack = createStackNavigator();
+
+const NotesNavigator = () => (
+  <Stack.Navigator>
+    {/* <Stack.Screen
+      name="NoteList"
+      component={NoteListScreen}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="NoteAdd"
+      component={NoteAddScreen}
+      options={{ headerShown: false }}
+    /> */}
+  </Stack.Navigator>
+);
+
+export default NotesNavigator;
