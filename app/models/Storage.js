e78ce33bac106abf5825028698bb2e1de6e94af5
ftomi <tomifarkas@yahoo.com>
2021-03-10 10:14:59 +0100

    default storage fix

--- app/models/Storage.js
+++ app/models/Storage.js
@@ -1,26 +1,26 @@
-import * as  SQLite from 'expo-sqlite'
-import { BaseModel, types } from 'expo-sqlite-orm'
+import * as SQLite from "expo-sqlite";
+import { BaseModel, types } from "expo-sqlite-orm";
 
 export default class Storage extends BaseModel {
-    constructor(obj) {
-        super(obj)
-    }
+  constructor(obj) {
+    super(obj);
+  }
 
-    static get database() {
-        return async () => SQLite.openDatabase('database.db')
-    }
+  static get database() {
+    return async () => SQLite.openDatabase("database.db");
+  }
 
-    static get tableName() {
-        return 'storages'
-    }
+  static get tableName() {
+    return "storages";
+  }
 
-    static get columnMapping() {
-        return {
-            id: { type: types.INTEGER, primary_key: true },
-            code: { type: types.TEXT, not_null: true },
-            name: { type: types.TEXT, not_null: true },
-            storeId: { type: types.INTEGER, not_null: true },
-            default: { type: types.BOOLEAN, not_null: true }
-        }
-    }
+  static get columnMapping() {
+    return {
+      id: { type: types.INTEGER, primary_key: true },
+      code: { type: types.TEXT, not_null: true },
+      name: { type: types.TEXT, not_null: true },
+      storeId: { type: types.INTEGER, not_null: true },
+      defaultStorage: { type: types.BOOLEAN, not_null: true },
+    };
+  }
 }
